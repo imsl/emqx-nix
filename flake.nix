4973a7dedd1a7990c9f6694203b10825e8a61439
Rickard Nilsson <rickynils@gmail.com>
2022-10-04 14:11:48 +0200

    emqx NixOS module: Don't run as root. Test actual MQTT traffic

--- flake.nix
+++ flake.nix
@@ -44,21 +44,40 @@
 
               imports = [ ./nixos/emqx.nix ];
 
+              networking.firewall.allowedTCPPorts = [ 1883 ];
+
               services.emqx = {
                 enable = true;
                 package = emqx;
+                nodeName = "emqx@127.0.0.1";
+                logConsoleHandler.level = "notice";
               };
 
             };
 
-            #client = { };
+            client = { pkgs, ... }: {
+
+              environment.systemPackages = with pkgs; [ mosquitto ];
+
+            };
 
           };
 
           testScript = ''
             start_all()
             server.wait_for_unit("emqx.service")
+
+            # Verify the MQTT listener comes up
             server.wait_for_open_port(1883)
+
+            # Verify the EMQX web UI listener comes up
+            server.wait_for_open_port(18083)
+
+            # Send a retained message to the EMQX broker
+            client.succeed("mosquitto_pub -h server -r -t test -m test")
+
+            # Receive the same message in a separate session
+            client.succeed("mosquitto_sub -h server -W 5 -C 1 -t test")
           '';
         };
 
--- nixos/emqx.nix
+++ nixos/emqx.nix
@@ -20,6 +20,17 @@ in {
         '';
       };
 
+      nodeName = lib.mkOption {
+        type = lib.types.str;
+        description = lib.mdDoc ''
+          The name of the EMQX node. Notice, this name is used for setting up
+          the EMQX database. If you change the node name, you need to purge
+	  `${cfg.stateDir}/data/mnesia` before starting the service with the
+          new name. See the EMQX
+          [documentation](https://www.emqx.io/docs/en/v5.0/deploy/install.html#package-installation-linux).
+        '';
+      };
+
       stateDir = lib.mkOption {
         type = lib.types.str;
         default = "/var/lib/emqx";
@@ -28,31 +39,74 @@ in {
         '';
       };
 
+      logDir = lib.mkOption {
+        type = lib.types.str;
+        default = "/var/log/emqx";
+        description = lib.mdDoc ''
+          Log directory EMQX will use.
+        '';
+      };
+
+      logConsoleHandler = {
+        enable = lib.mkEnableOption (lib.mdDoc "console logging");
+        level = lib.mkOption {
+          type = lib.types.enum [
+            "debug" "info" "notice" "warning" "error" "critical" "alert"
+            "emergency"
+          ];
+          default = "warning";
+          description = lib.mdDoc ''
+            The log level used for EMQX console logs
+          '';
+        };
+      };
+
     };
 
   };
 
   config = lib.mkIf cfg.enable {
 
+    services.emqx.logConsoleHandler.enable = lib.mkDefault true;
+
     systemd.services.emqx = {
       description = "emqx daemon";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
       path = with pkgs; [
-        bash gawk inetutils
+        bash gawk inetutils rsync
       ];
       environment = {
+        EMQX_NODE_NAME = cfg.nodeName;
         EMQX_NODE__DATA_DIR = cfg.stateDir;
+        EMQX_LOG__CONSOLE_HANDLER__ENABLE =
+          if cfg.logConsoleHandler.enable then "true" else "false";
+        EMQX_LOG__CONSOLE_HANDLER__LEVEL = cfg.logConsoleHandler.level;
+        # We're using upstream OTP, not the EMQX fork, so we must use mnesia
+        # See https://github.com/emqx/emqx/discussions/8592
+        EMQX_NODE__DB_BACKEND = "mnesia";
       };
+      preStart = ''
+        # We need to copy the entire emqx package into the state dir because
+        # EMQX expects some files to be writeable
+        rsync -r --del --chmod=ug+w \
+          "${cfg.package}/dist/" "${cfg.stateDir}/dist/"
+        echo 'RUNNER_LOG_DIR="${cfg.logDir}"' >> \
+          "${cfg.stateDir}/dist/emqx/rel/emqx/releases/emqx_vars"
+      '';
       serviceConfig = {
-        ExecStart = "${cfg.package}/bin/emqx foreground";
+        ExecStart = "${cfg.stateDir}/dist/emqx/rel/emqx/bin/emqx foreground";
         LimitNOFILE = 1048576;
         TimeoutStopSec = "120s";
         Restart = "on-failure";
         RestartSec = "120s";
-
         StateDirectory =
           lib.mkIf (cfg.stateDir == "/var/lib/emqx") "emqx";
+        LogsDirectory =
+          lib.mkIf (cfg.logDir == "/var/log/emqx") "emqx";
+        DynamicUser = true;
+        User = "emqx";
+        Group = "emqx";
       };
     };
 
